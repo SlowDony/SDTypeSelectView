4f6c8885beed3661d2790e22e1b0c4afc3fd01b4
吕东 <435574793@qq.com>
2016-12-31 05:07:07 +0800

    1231创建

---
+++ SDTypeSelectView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDTypeSelectView
+//
+//  Created by apple on 2016/12/31.
+//  Copyright © 2016年 slowdony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDTypeSelectView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDTypeSelectView
+//
+//  Created by apple on 2016/12/31.
+//  Copyright © 2016年 slowdony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
